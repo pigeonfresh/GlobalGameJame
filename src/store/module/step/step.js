e07bed562ef51729474362683c5b1d48816545d7
Gian F. Olivieri <gian.olivieri@mediamonks.com>
2019-01-26 12:23:58 -0300

    moves up and down

--- src/store/module/step/step.js
+++ src/store/module/step/step.js
@@ -45,13 +45,43 @@ export default {
         .on(
           d => d === DIRECTIONS.UP,
           () => {
-            /* do stuff */
+            const currentStep = HOUSE[state[CURRENT_FLOOR]][state[CURRENT_STEP]];
+            match(currentStep)
+              .on(
+                s => s === ROOMS.BOTTOM_STAIRS,
+                () => {
+                  state[CURRENT_FLOOR] = 1;
+                  state[CURRENT_STEP] = HOUSE[1].findIndex(r => r === ROOMS.MIDDLE_STAIRS_2);
+                },
+              )
+              .on(
+                s => s === ROOMS.MIDDLE_STAIRS_1,
+                () => {
+                  state[CURRENT_FLOOR] = 0;
+                  state[CURRENT_STEP] = HOUSE[0].findIndex(r => r === ROOMS.TOP_STAIRS);
+                },
+              );
           },
         )
         .on(
           d => d === DIRECTIONS.DOWN,
           () => {
-            /* do stuff */
+            const currentStep = HOUSE[state[CURRENT_FLOOR]][state[CURRENT_STEP]];
+            match(currentStep)
+              .on(
+                s => s === ROOMS.TOP_STAIRS,
+                () => {
+                  state[CURRENT_FLOOR] = 1;
+                  state[CURRENT_STEP] = HOUSE[1].findIndex(r => r === ROOMS.MIDDLE_STAIRS_1);
+                },
+              )
+              .on(
+                s => s === ROOMS.MIDDLE_STAIRS_2,
+                () => {
+                  state[CURRENT_FLOOR] = 2;
+                  state[CURRENT_STEP] = HOUSE[2].findIndex(r => r === ROOMS.BOTTOM_STAIRS);
+                },
+              );
           },
         );
     },
