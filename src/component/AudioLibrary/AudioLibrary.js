c13ea4f13331dd0bc1b864073e617586271c7542
eelke <eelkevanderbij.prive+github@gmail.com>
2019-01-26 18:56:06 -0300

    Add shizzle

--- src/component/AudioLibrary/AudioLibrary.js
+++ src/component/AudioLibrary/AudioLibrary.js
@@ -13,11 +13,29 @@ export default {
   },
   mounted() {
     this.soundFX = new Howl({
-      src: [`${this.$staticRoot}audio/theme-song.mp3`],
+      src: [`${this.$staticRoot}audio/sfx.mp3`],
       sprite: {
-        blast: [0, 3000],
-        laser: [4000, 1000],
-        winner: [6000, 5000],
+        meow: [0, 2000],
+        purr: [2000, 6000],
+        nom: [6000, 7000],
+        petting: [7000, 9000],
+        doorbell: [9000, 11000],
+        phone: [11000, 12000],
+        footstep: [12000, 13000],
+        litter: [13000, 14000],
+        scratching: [14000, 16000],
+        fire: [160000, 17000],
+        vasePut: [0, 1000],
+        vaseBreaking: [0, 1000],
+        Faucet: [0, 1000],
+        waterRunning: [0, 1000],
+        bathTub: [0, 1000],
+        boxesFalling: [0, 1000],
+        boxesWithGlasses: [0, 1000],
+        rooster: [0, 1000],
+        owl: [0, 1000],
+        clock: [0, 1000],
+        clockFast: [0, 1000],
       },
     });
 
